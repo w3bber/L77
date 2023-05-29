7270b1f15f78861a9d5eb9e841477fce7a4fb05e
Alexander Shiryaev <alexandr.shiryaev.04@gmail.com>
2023-05-29 09:57:17 +0300

    check in main

--- WeatherService/JsonService.cpp
+++ WeatherService/JsonService.cpp
@@ -16,8 +16,8 @@ Weather JsonService::getWeather(string s)
     std::string city = j["name"]; //  Киров
     double lon = j["coord"]["lon"]; // 49.6601
     double lat = j["coord"]["lat"]; // 58.5966  
-    double temperature = j["temp"]; // 5.69
-    std::string weather = j["weather"]["description"]; // дождь
+    double temperature = j["main"]["temp"]; // 5.69
+    std::string weather = j["weather"][0]["description"]; // дождь
     double windSpeed = j["wind"]["speed"]; // 4.27
     int clouds = j["clouds"]["all"]; // 100
     return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,1 +1,9 @@
-﻿
+﻿#include "Weather.h"
+#include "Service.h"
+#include "JsonService.h"
+
+int main()
+{
+	JsonService js;
+	Weather w = js.getWeather("weather.json");
+}
